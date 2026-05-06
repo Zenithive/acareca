5369a67f19e445af79582fbc1f3cd31b60d4c63e
mihirk-khode <mihirk@zenithive.com>
2026-05-06 17:10:37 +0530

    refactor(migrations): Remove image_url column addition from clinic migration

--- migrations/20260506112313_alter_clinic.sql
+++
@@ -1,5 +0,0 @@
--- Add image_url column to tbl_clinic table
-ALTER TABLE tbl_clinic ADD COLUMN IF NOT EXISTS image_url TEXT;
-
--- Add comment to the column
-COMMENT ON COLUMN tbl_clinic.image_url IS 'URL of the clinic image';
