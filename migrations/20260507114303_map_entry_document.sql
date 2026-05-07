96b642768eae7dc920daae23188aca6c03e1b95a
mihirk-khode <mihirk@zenithive.com>
2026-05-07 18:22:18 +0530

    fix(migrations): Rename form_entry_id to entry_id and update unique constraint in tbl_map_entry_document

--- migrations/20260507114303_map_entry_document.sql
+++ migrations/20260507114303_map_entry_document.sql
@@ -3,7 +3,7 @@
 CREATE TABLE tbl_map_entry_document (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
 
-    form_entry_id UUID NOT NULL
+    entry_id UUID NOT NULL
         REFERENCES tbl_form_entry(id),
     document_id UUID NOT NULL
         REFERENCES tbl_document(id),
@@ -11,11 +11,11 @@ CREATE TABLE tbl_map_entry_document (
     created_by UUID,
     created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
 
-    UNIQUE(form_entry_id, document_id)
+    CONSTRAINT tbl_map_entry_document_entry_id_document_id_key UNIQUE(entry_id, document_id)
 );
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-SELECT 'down SQL query';
+DROP TABLE IF EXISTS tbl_map_entry_document;
 -- +goose StatementEnd
