eab84ef7643bb71bcb7ff78cdb3ccd06e7e846df
mihirk-khode <mihirk@zenithive.com>
2026-05-19 12:31:48 +0530

    feat(auth): Implement initial authentication handler and route registration

--- internal/modules/clinic/auth/handler.go
+++ internal/modules/clinic/auth/handler.go
@@ -1,1 +1,25 @@
 package auth
+
+import "github.com/gin-gonic/gin"
+
+type IHandler interface {
+	Login(c *gin.Context)
+	Register(c *gin.Context)
+}
+
+type Handler struct {
+}
+
+func NewHandler() IHandler {
+	return &Handler{}
+}
+
+// Login implements [IHandler].
+func (h *Handler) Login(c *gin.Context) {
+	panic("unimplemented")
+}
+
+// Register implements [IHandler].
+func (h *Handler) Register(c *gin.Context) {
+	panic("unimplemented")
+}
--- internal/modules/clinic/auth/route.go
+++ internal/modules/clinic/auth/route.go
@@ -1,1 +1,13 @@
 package auth
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterRoutes(r *gin.Engine, h IHandler) {
+	auth := r.Group("/auth")
+	{
+		auth.POST("/login", h.Login)
+		auth.POST("/register", h.Register)
+	}
+}
