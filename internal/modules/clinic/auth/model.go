ce124a5dacd037542d8f0852f5adbf78ad903827
mihirk-khode <mihirk@zenithive.com>
2026-05-19 12:35:56 +0530

    feat(auth): Add request and response models for clinic authentication

--- internal/modules/clinic/auth/model.go
+++ internal/modules/clinic/auth/model.go
@@ -1,1 +1,15 @@
 package auth
+
+type RqClinic struct {
+	DocumentId *string `json:"document_id"`
+	Name       string  `json:"name" validate:"required, min=3, max=100"`
+	Email      string  `json:"email" validate:"required,email"`
+	Password   string  `json:"password" validate:"required,min=8"`
+}
+
+type RsClinic struct {
+	ID         string  `json:"id"`
+	DocumentId *string `json:"document_id,omitempty"`
+	Name       string  `json:"name"`
+	Email      string  `json:"email"`
+}
